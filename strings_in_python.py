77c97fc640682cb8833e4f27c27a46407f3c5114
RobertOrtmann <rob.ortmann@gmail.com>
2024-03-19 14:31:21 +0100

    third

--- strings_in_python.py
+++ strings_in_python.py
@@ -36,3 +36,25 @@ print(second_octet)
 subnet_mask = cidr_notation.split('/') [1]
 print(subnet_mask)
 
+greeting = "Hello, Python!"
+sub_greeting = greeting[0:5]
+print(sub_greeting)
+
+language = greeting[7:]
+print(language)
+
+skip_chars = greeting[::2] # extracts'Hlo yhn'
+print(skip_chars)
+
+reversed_greeting = greeting[::-1] # reverse the string
+print(reversed_greeting)
+
+last_chars = greeting[-7:]
+print(last_chars)
+
+middle_chars = greeting[2:-1] 
+print(middle_chars)
+
+nth_chars = greeting[1:10:3]
+print(nth_chars)
+
