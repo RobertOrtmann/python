793382bf6e9e306953f8f0aa138ca3e717c8dd77
RobertOrtmann <rob.ortmann@gmail.com>
2024-03-19 13:52:49 +0100

    second

--- basic_operatrions.py
+++ basic_operatrions.py
@@ -45,6 +45,39 @@ for row in matrix:
 for row in matrix:
     print(' '.join(map(str, row)))
 
+my_string = "Hello, Python!"
+try:
+    my_string[7] = 'W' # Trying to replace 'P' with 'W'
+except TypeError as e:
+    print(e) #strings are imutable in python
+
+my_list = [1, 2, 3, 4]
+my_list[2] = 30 # Changing the third element from 3 to 30
+print(my_list)
+
+original_list = [1, 2, 3]
+new_list = original_list # this copies the reference not the list
+new_list[1] = 200 # modifies the original list as well
+print(original_list)
+
+
+independent_copy = original_list[ : ]
+independent_copy[1] = 20
+print(original_list)
+print(independent_copy)
+
+a = 10
+b = a
+b = 5 # 'a' remains unchanged
+print(a)
+print(b)
+
+
+lst1 = [1, 2, 3]
+lst2 = lst1
+lst2.append(4)
+print(lst1)
+
 
 
 
---
+++ strings_in_python.py
@@ -0,0 +1,38 @@
+single_quoted_string = 'Hello, Python!'
+double_quoted_string = "Hello, Python!"
+print(single_quoted_string)
+print(double_quoted_string)
+
+greeting = "Hello"
+target = "World"
+message = greeting + ", " + target + "!"
+print(message)
+
+name = "Python"
+version = 3.8
+message = f"Welcome to {name} version {version} !"
+print(message)
+
+a = 5
+b = 10
+result_message = f"The sum of {a} and {b} is {a + b}."
+print(result_message)
+
+length = len(message)
+print(length)
+
+cidr_notation = "172.168.1.1/24"
+ip_address = cidr_notation.split ('/') [0]
+print(ip_address)
+
+first_octet = ip_address[ :ip_address.index('.')]
+print(first_octet)
+
+second_octet_start = ip_address.index('.') + 1
+second_octet_end = ip_address.index('.', second_octet_start)
+second_octet = ip_address[ second_octet_start:second_octet_end ]
+print(second_octet)
+
+subnet_mask = cidr_notation.split('/') [1]
+print(subnet_mask)
+
