367a577d47ae392df0222842aa16056cf3bb7d6a
RobertOrtmann <rob.ortmann@gmail.com>
2024-03-20 15:26:25 +0100

    ten

---
+++ object_oriented.py
@@ -0,0 +1,18 @@
+class NetworkDevice :
+    def __init__(self, hostname, ip_address) :
+        self.hostname = hostname
+        self.ip_address = ip_address
+
+router = NetworkDevice ("Router1", "192.168.1.1")
+print(f"Device: {router.hostname}, IP: {router.ip_address}")
+
+class NetworkDevice :
+    def __init__(self, hostname, ip_address) :
+        self.hostname = hostname
+        self.ip_address = ip_address
+
+def display_config(self) :
+    print(f"Hostname: {self.hostname}, IP Address: {self.ip_address}")
+
+router.display_config()
+
